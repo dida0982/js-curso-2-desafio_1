cffdc5094c05a2a740e356b39297de0d02f66eba
dida0982 <dida0982@gmail.com>
2026-04-30 11:47:45 -0300

    feat: I added a question and an alert that links to the answer to the question.
    Signed-off-by: dida0982 <dida0982@gmail.com>

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 let title = document.querySelector( 'h1' );
 title.innerHTML= 'Time for the challenger'
 
-function consoleClick(params) {
-console.log('The button was clicked');
+function consoleClick ( params )
+{
+    console.log('The button was clicked');
 }
 
 function alertClick(params) {
     alert( `I love JS` );
+}
+
+function promptClick(params) {
+    let city = prompt( `Enter a city of de Brazil` );
+    alert( `I was in the ${ city } and i thought of you` );
 }
